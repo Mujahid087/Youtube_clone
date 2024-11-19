ebedc3a7c0945d8ad3079841cf694a64701eeb9c
Mujahid087 <mujahidmahedi10@gmail.com>
2024-11-19 14:53:06 +0530

    added the feed

--- EchoStudio/src/pages/Home/Home.jsx
+++ EchoStudio/src/pages/Home/Home.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import './Home.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import Feed from '../../Components/Feed/Feed';
 const Home = ({sidebar}) => {
   return (
     <>
       <Sidebar sidebar={sidebar}/>
+      <div className={`container ${sidebar?"":'large-container'}`}>\
+        <Feed/>
+      </div>
     
     </>
   )
