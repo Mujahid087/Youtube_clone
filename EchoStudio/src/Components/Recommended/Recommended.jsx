8b4b2ac6fb6a8693e9c798ea0b8b9eafee88a777
Mujahid087 <mujahidmahedi10@gmail.com>
2024-11-26 10:42:47 +0530

    completed the project

--- EchoStudio/src/Components/Recommended/Recommended.jsx
+++ EchoStudio/src/Components/Recommended/Recommended.jsx
@@ -21,7 +21,7 @@ const Recommended = ({ categoryId }) => {
 
     // Utility function to truncate titles
     const truncateTitle = (title, maxLength) => {
-        return title.length > maxLength ? `${title.slice(0, maxLength)}...` : title;
+        return title.length > maxLength ? `${title.slice(0, 15)}...` : title;
     };
 
     return (
@@ -53,57 +53,4 @@ Recommended.propTypes = {
 export default Recommended;
 
 
-
-// import React, { useEffect, useState } from "react";
-// import "./Recommended.css";
-// import PropTypes from "prop-types";
-// import { API_KEY, value_converter } from "../../data";
-
-// const Recommended = ({ categoryId }) => {
-//   const [apiData, setApiData] = useState([]);
-
-//   // Fetching data from the YouTube API
-//   const fetchData = async () => {
-//     const relatedVideoUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${API_KEY}&maxResults=45`;
-//     try {
-//       const response = await fetch(relatedVideoUrl);
-//       const data = await response.json();
-//       setApiData(data.items || []); // Handle cases where `items` may be undefined
-//     } catch (error) {
-//       console.error("Error fetching YouTube API data:", error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []); // Re-fetch videos when `categoryId` changes
-
-//   return (
-//     <div className="recommended">
-//       {apiData.map((item) => {
-//         const { id, snippet, statistics } = item;
-//         const { title, channelTitle, thumbnails } = snippet;
-//         return (
-//           <div key={id} className="side-video-list">
-//             <img
-//               src={thumbnails?.medium?.url || thumbnails?.default?.url}
-//               alt={title}
-//             />
-//             <div className="vid-info">
-//               <h4>{title}</h4>
-//               <p>{channelTitle}</p>
-//               <p>{value_converter(statistics?.viewCount)} Views</p>
-//             </div>
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// };
-
-// Recommended.propTypes = {
-//   categoryId: PropTypes.string.isRequired,
-// };
-
-// export default Recommended;
 
