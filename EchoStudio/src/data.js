a3badcb180add70bed64b2083db5a104953e9d0d
Mujahid087 <mujahidmahedi10@gmail.com>
2024-11-26 10:48:54 +0530

    project completed

--- EchoStudio/src/data.js
+++ EchoStudio/src/data.js
@@ -1,5 +1,6 @@
 export const API_KEY='AIzaSyARchGtl1vp62ryU5sieEh-Faom3TcnQrg';
 export const value_converter = (value) => {
+
     if (value >= 1000000) {
         return Math.floor(value / 1000000) + "M";
     } else if (value >= 1000) {
